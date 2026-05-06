4e5b3dcabc85cffdc9d50214f400c80129d4bfd5
eng-oumayma <oumaymahadjmohamed@gmail.com>
2026-05-06 23:47:08 +0100

    fix: use environment apiUrl for backend

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://projet-cloud-alb-41845803.us-east-1.elb.amazonaws.com'  // vide = même domaine que le frontend
+  apiUrl: 'http://projet-cloud-alb-997019568.us-east-1.elb.amazonaws.com' // vide = même domaine que le frontend
 };
